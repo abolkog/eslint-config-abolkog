5f8ae2a2f152b7d5a036c184e4eb2af150df1a73
Khalid Elshafie <abolkog@gmail.com>
2017-11-10 12:38:02 +0900

    Fixing invalid rule in react

--- rules/react.js
+++ rules/react.js
@@ -157,7 +157,7 @@ module.exports = {
   
       // Prevent missing props validation in a React component definition
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/prop-types.md
-      'react/prop-types': ['error', { 'ignore': [], 'customValidators': [], skipUndeclared: false }],
+      'react/prop-types': ['error', { ignore: [], customValidators: [] }],
   
       // Prevent missing React when using JSX
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
