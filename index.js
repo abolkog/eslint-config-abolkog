0edb07f250eeda790ff76bd88e7fae19040c5939
Khalid Elshafie <abolkog@gmail.com>
2017-11-10 11:37:29 +0900

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = {
+    extends: [
+      './rules/es6'
+    ].map(require.resolve),
+    parserOptions: {
+      ecmaVersion: 7,
+      sourceType: 'module'
+    },
+    plugins: [
+      'class-property',
+      'import'
+    ],
+    parser: 'babel-eslint',
+    rules: {
+        'no-use-before-define': 0,
+        'strict': 0,
+        'react/jsx-indent': 0,
+        'react/jsx-filename-extension': 0,
+        'react/prop-types': 0,
+        'react/no-did-mount-set-state': 1,
+        'react/prefer-stateless-function': 0,
+        'no-trailing-spaces': 0,
+        'import/prefer-default-export': 0,
+        'import/no-extraneous-dependencies': 0,
+        'new-cap': 0,
+        'func-names': 0,
+        'consistent-return': 0,
+        'global-require': 0,
+        'linebreak-style': 0,
+      
+    },
+    globals: {
+      "describe": false,
+      "it": false,
+      "test": false,
+      "expect": false,
+      "beforeEach": false,
+      "afterEach": false,
+      "before": false
+    }
+  };
